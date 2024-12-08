55db76c71ddfe74d36dd872068160247132ea668
Sebastian Florez <sebasflorez69@gmail.com>
2024-12-08 12:16:41 -0500

    feat: event handler implemented

--- src/event.rs
+++ src/event.rs
@@ -1,2 +1,32 @@
-// TODO: Implement event management module
-todo!();
+use crossterm::event::{self, Event, KeyCode, KeyEvent};
+use std::time::Duration;
+
+pub enum AppEvent {
+    Quit,
+    Tick,
+    Key(KeyEvent),
+}
+
+pub struct EventHandler;
+
+impl EventHandler {
+    pub fn handle_events(&self, timeout: Duration) -> Result<AppEvent, std::io::Error> {
+        if crossterm::event::poll(timeout)? {
+            match event::read()? {
+                Event::Key(key) => match key.code {
+                    KeyCode::Char('q') => Ok(AppEvent::Quit),
+                    _ => Ok(AppEvent::Key(key)),
+                },
+                _ => Ok(AppEvent::Tick),
+            }
+        } else {
+            Ok(AppEvent::Tick)
+        }
+    }
+}
+
+// TODO implement test for events management
+#[cfg(test)]
+mod tests {
+    todo!();
+}
