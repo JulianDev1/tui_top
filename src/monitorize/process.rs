300b9dc9bdc8bb71c9f90275931d3999104016cc
pabrax <pabloeg0420@outlook.com>
2024-12-28 17:31:55 -0500

    process module | not found

--- src/monitorize/process.rs
+++ src/monitorize/process.rs
@@ -1,39 +1,75 @@
-
-use sysinfo;
+use sysinfo::{System, ProcessesToUpdate, ProcessRefreshKind};
 
 struct ProcessInfo {
-    pid: u8,
+    pid: u32,
     name: String,
-    cpu_usage: f32,
-    user_id: u8,
-
+    cpu: f32,
+    memory: u64,
+    user: String
 }
 
 struct ProcessesList {
-    processes: Vec<ProcessInfo>
+    pub system: System,
+    pub processes: Vec<ProcessInfo>
 }
 
-impl ProcessInfo {
+impl ProcessesList {
 
+    pub fn new(system: System) -> Self {
+        
+        let processes = system.processes();
+        
+        // ! ERROR: cannot borrow `system` as mutable because it is also borrowed as immutable
+        ProcessesList {
+            system,
+            processes: processes.iter().map(|(_pid, process)| {
+                ProcessInfo {
+                    pid: process.pid().as_u32(),
+                    name: process.name().to_string_lossy().into_owned(),
+                    cpu: process.cpu_usage(),
+                    memory: process.memory(),
+                    user: process.user_id().map_or_else(|| "Unknown".to_string(), |uid| uid.to_string())
+                }
+            }).collect()
+        }
+    }
     
+    fn get_processes(&self) -> &Vec<ProcessInfo> {
+        &self.processes
+    }
+    
+    fn refresh_processes(&mut self) {
+        self.system.refresh_processes_specifics(ProcessesToUpdate::All, true, ProcessRefreshKind::everything() );
+    }
+
 }
+
+
 
 
 pub fn view() {
-    let mut sys = sysinfo::System::new_all();
-    sys.refresh_all();
+    let mut sys = System::new_all();
+    
+    let mut processes_list = ProcessesList::new(sys);
 
+    for process in processes_list.get_processes() {
+        println!("PID {} | Nombre {} | CPU {} | memoria {} | user {}", 
+            process.pid,
+            process.name,
+            process.cpu,
+            process.memory,
+            process.user
+        );
+    }
+    processes_list.refresh_processes();
 
-    for proc in sys.processes() {
-        let process = proc.1; // select the &Process
-        println!(
-            "PID: {} | Nombre: {} | CPU: {} | memoria: {} | user {:?} ",
-            process.pid(),    
-            process.name().to_string_lossy(),
-            process.cpu_usage(),
-            process.memory(),
-            process.user_id()
-            
+    for process in processes_list.get_processes() {
+        println!("PID {} | Nombre {} | CPU {} | memoria {} | user {}", 
+            process.pid,
+            process.name,
+            process.cpu,
+            process.memory,
+            process.user
         );
     }
 }
