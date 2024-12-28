a792369c9e12b88224e9950ab2d142eeda58f905
pabrax <pabloeg0420@outlook.com>
2024-12-28 18:28:03 -0500

    module memoria | add struct

--- src/monitorize/memoria.rs
+++ src/monitorize/memoria.rs
@@ -1,14 +1,50 @@
-use sysinfo::System;
+use sysinfo::{MemoryRefreshKind, System};
+
+
+struct MemoryInfo {
+    system: System,
+    free: u64,
+    used: u64,
+    total: u64
+}
+
+impl MemoryInfo {
+
+    pub fn new(system: System) -> Self {
+
+        MemoryInfo {
+            system: System::new_all(),
+            used: system.used_memory(),
+            total: system.total_memory(),
+            free: system.total_memory() - system.used_memory(),
+        }
+    }
+
+    pub fn refresh(&mut self) {
+        self.system.refresh_memory_specifics(MemoryRefreshKind::everything());
+        self.free = self.system.total_memory() - self.system.used_memory();
+        self.used = self.system.used_memory();
+    }
+}
+
 
 pub fn view() {
 
-    let mut sys = System::new_all();
-    sys.refresh_all();
+    let sys = System::new_all();
     
-    println!(
-        "free: {} | used: {} | Total {} ",
-        sys.free_memory() / 1024 / 1024,
-        sys.used_memory() / 1024 / 1024,
-        sys.total_memory() / 1024/ 1024,
+    let mut memory = MemoryInfo::new(sys);
+
+    println!("free: {} | used {} | total {} ",
+        memory.free / 1024 / 1024, 
+        memory.used / 1024 / 1024,
+        memory.total / 1024 / 1024
+    );
+
+    memory.refresh();
+
+    println!("free: {} | used {} | total {} ",
+        memory.free / 1024 / 1024, 
+        memory.used / 1024 / 1024,
+        memory.total / 1024 / 1024
     );
 }
