71a9e4ae4ff10f796d0692bb0a8c5ccea3bff6e0
Sebastian Florez <sebasflorez69@gmail.com>
2024-12-29 12:20:20 -0500

    feat: general information collector refactored

--- src/monitorize/general_information.rs
+++ src/monitorize/general_information.rs
@@ -1,8 +1,31 @@
 use sysinfo::System;
 
-pub fn view(){
-    println!("System Name:             {:?}", System::name());
-    println!("System kernel version:   {:?}", System::kernel_version());
-    println!("System OS version:       {:?}", System::os_version());
-    println!("System host name:        {:?}", System::host_name());
-} 
+pub struct GeneralInfo {
+    name: String,
+    os_version: String,
+    long_os_version: String,
+    kernel_version: String,
+    host_name: String,
+}
+
+impl GeneralInfo {
+    pub fn new() -> Self {
+        GeneralInfo {
+            name: System::name().unwrap(),
+            os_version: System::os_version().unwrap(),
+            long_os_version: System::long_os_version().unwrap(),
+            kernel_version: System::kernel_version().unwrap(),
+            host_name: System::host_name().unwrap(),
+        }
+    }
+}
+
+pub fn view() {
+    let info = GeneralInfo::new();
+
+    println!("System Name:             {:?}", info.name);
+    println!("System kernel version:   {:?}", info.kernel_version);
+    println!("System OS version:       {:?}", info.os_version);
+    println!("System host name:        {:?}", info.host_name);
+    println!("System long OS version:  {:?}", info.long_os_version);
+}
