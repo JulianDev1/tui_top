de1e6b3ea4b41677c4a9561e8cea85c99752d024
Sebastian Florez <sebasflorez69@gmail.com>
2024-12-29 15:57:19 -0500

    feat: disk metrics collector refactored

--- src/monitorize/disks.rs
+++ src/monitorize/disks.rs
@@ -1,11 +1,72 @@
-use sysinfo::{System, Disks};
+use sysinfo::Disks;
 
-pub fn view(){
-    let mut disks = Disks::new_with_refreshed_list();
+pub struct LocalDisk {
+    pub mount_point: String,
+    pub name: String,
+    pub kind: String,
+    pub total_space: u64,
+    pub available_space: u64,
+}
 
-    for disk in &disks {
+pub struct DisksList {
+    pub disks: Vec<LocalDisk>,
+}
+
+impl DisksList {
+    pub fn new(disks: Disks) -> Self {
+        let disks = Disks::new_with_refreshed_list()
+            .iter()
+            .map(|disk| LocalDisk {
+                mount_point: disk.mount_point().to_str().unwrap().to_owned(),
+                name: disk
+                    .name()
+                    .to_str()
+                    .unwrap_or_else(|| "Local Disk")
+                    .to_owned(),
+                kind: disk.kind().to_string(),
+                total_space: disk.total_space(),
+                available_space: disk.available_space(),
+            })
+            .collect();
+
+        DisksList { disks }
+    }
+
+    pub fn update_disks(&mut self) {
+        let disks = Disks::new_with_refreshed_list();
+        self.disks = disks
+            .iter()
+            .map(|disk| LocalDisk {
+                mount_point: disk.mount_point().to_str().unwrap().to_owned(),
+                name: disk
+                    .name()
+                    .to_str()
+                    .unwrap_or_else(|| "Local Disk")
+                    .to_owned(),
+                kind: disk.kind().to_string(),
+                total_space: disk.total_space(),
+                available_space: disk.available_space(),
+            })
+            .collect();
+    }
+}
+
+pub fn view() {
+    let disks = Disks::new_with_refreshed_list();
+    let mut disks_list = DisksList::new(disks);
+
+    for disk in disks_list.disks.iter() {
         println!(
-            "{disk:?}", 
+            "Disk: {}{} | Kind: {} | Total Space: {:.2} GB | Available Space: {:.2} GB",
+            disk.mount_point,
+            disk.name,
+            disk.kind,
+            bytes_to_gb(disk.total_space),
+            bytes_to_gb(disk.available_space)
         );
     }
+}
+
+pub fn bytes_to_gb(bytes: u64) -> f32 {
+    bytes as f32 / 1024.0 / 1024.0 / 1024.0
 }
