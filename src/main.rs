fec9ff20305b075382e76f20201ddbd2d8902e50
Sebastian Florez <sebasflorez69@gmail.com>
2024-12-30 11:30:30 -0500

    fix: disk object in constructor fixed

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,12 @@
-use system_info::SystemMetrics;
-
-mod event;
-mod system_info;
-
 mod monitorize;
 
-use monitorize::{general_information, process, cpu, memoria, disks};
+use monitorize::{general_information, process, cpu, memory, disks};
 
 fn main () {
-    memoria::view();
+    memory::view();
     cpu::view();
     process::view();
     disks::view();
     general_information::view();
 
 }
-
-
-//fn main() {
-//    let mut sis = system_info::SystemInfoCollector::new();
-//
-//    //println!("{:?}", sis.collect_metrics());
-//    let memory_used = sis.collect_metrics().memory_used;
-//    let memory_used = (memory_used / 1024.0 / 1024.0);
-//    println!("{}", memory_used);
-//
-//}
